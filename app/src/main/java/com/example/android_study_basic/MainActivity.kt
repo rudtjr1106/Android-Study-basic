e43138b43661f786c2de74a6038a2b9d3a653c02
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2023-11-01 16:14:04 +0900

    commit - 11/1
    databinding 활용

--- app/src/main/java/com/example/android_study_basic/MainActivity.kt
+++ app/src/main/java/com/example/android_study_basic/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.android_study_basic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.android_study_basic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    var counter : String = "0"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.vm = this
+        binding.textViewCounter.text = counter
+        setButton()
+    }
+
+    fun setButton(){
+        binding.apply {
+            buttonReset.setOnClickListener {
+                counter = "0"
+                textViewCounter.text = counter
+                println(counter)
+            }
+
+            buttonPlus.setOnClickListener {
+                counter = (counter.toInt()+1).toString()
+                textViewCounter.text = counter
+                println(counter)
+            }
+        }
     }
 }
